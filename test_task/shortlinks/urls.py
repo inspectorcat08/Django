fbe3d38afe30abe2cde44c02cc54e4c535e81e2a
inspectorcat <75495658+inspectorcat08@users.noreply.github.com>
2021-09-18 11:56:32 +0300

    Delete test_task directory

--- test_task/shortlinks/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import home_view, redirect_url_view
-
-appname = "shortlinks"
-
-urlpatterns = [
-    path('', home_view, name='home'),
-    path('<str:shortened_part>', redirect_url_view, name='redirect'),
-]
